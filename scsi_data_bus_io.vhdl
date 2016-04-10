5407971e653f449a81c7b99a73745bb55bc58a46
Pat Gavlin <pgavlin@gmail.com>
2016-04-10 12:20:20 -0700

    Redesign the data bus IO unit as a synchronous component.

--- scsi_data_bus_io.vhdl
+++ scsi_data_bus_io.vhdl
@@ -4,153 +4,130 @@ use ieee.std_logic_1164.all;
 entity scsi_data_bus_io is
 	port(
 		-- Internal bus signals
-		rst   : in std_logic;
-		sel   : in std_logic;
-		rw    : in std_logic;
-		busy  : out std_logic;
-		err   : out std_logic;
-		data  : inout std_logic_vector(7 downto 0);
+		rst : in std_logic;
+		clk : in std_logic;
+		write_enable : in std_logic;
+		output_enable : in std_logic;
+		direction : in std_logic;
+		busy : out std_logic;
+		err : out std_logic;
+		data_in : in std_logic_vector(7 downto 0);
+		data_out : out std_logic_vector(7 downto 0);
 
 		-- SCSI bus signals
 		scsi_req : out std_logic;
-		scsi_io  : out std_logic;
 		scsi_ack : in std_logic;
-		scsi_db  : inout std_logic_vector(7 downto 0);
+		scsi_db : inout std_logic_vector(7 downto 0);
 		scsi_dbp : inout std_logic
 	);
 begin
 end;
 
 architecture behavioral of scsi_data_bus_io is
-	signal sel_rising_edge : std_logic;
-	signal sel_rising_edge_en : std_logic;
-	signal sel_rising_edge_rst : std_logic;
+	-- Control signals
+	signal input_write_enable : std_logic;
+	signal output_write_enable : std_logic;
+	signal scsi_output_enable : std_logic;
+	signal pending_reg_next : std_logic;
+	signal busy_reg_next : std_logic;
+	signal req_reg_next : std_logic;
 
-	signal scsi_ack_rising_edge : std_logic;
-	signal scsi_ack_rising_edge_en : std_logic;
-	signal scsi_ack_rising_edge_rst : std_logic;
+	-- Data registers
+	signal input_data_reg : std_logic_vector(7 downto 0);
+	signal output_data_reg : std_logic_vector(7 downto 0);
+	signal err_reg : std_logic;
 
-	signal scsi_ack_falling_edge : std_logic;
-	signal scsi_ack_falling_edge_en : std_logic;
-	signal scsi_ack_falling_edge_rst : std_logic;
+	-- Control registers
+	signal direction_reg : std_logic; -- 1 = input, 0 = output
+	signal pending_reg : std_logic;
+	signal busy_reg : std_logic;
+	signal req_reg : std_logic;
 
-	signal in_progress : std_logic;
-	signal in_progress_rst : std_logic;
-
-	signal req : std_logic;
-	signal req_rst : std_logic;
-
-	signal direction : std_logic;
-
-	signal parity_sel : std_logic;
+	-- Parity generation
 	signal parity_data : std_logic_vector(7 downto 0);
 	signal parity : std_logic;
-
-	signal data_out : std_logic_vector(7 downto 0);
-	signal err_out : std_logic;
+	signal parity_err : std_logic;
 begin
-	busy <= in_progress;
-	scsi_req <= req;
-	scsi_io <= direction;
+	-- Control signals
+	input_write_enable <= not busy_reg and write_enable;
+	output_write_enable <= busy_reg and not req_reg and not scsi_ack and direction;
+	scsi_output_enable <= ((pending_reg and not pending_reg_next) or (not pending_reg and busy_reg)) and not direction and output_enable;
+	pending_reg_next <= input_write_enable or (pending_reg and req_reg and not scsi_ack);
+	busy_reg_next <= pending_reg or (busy_reg and not pending_reg and not req_reg_next);
+	--busy_reg_next <= pending_reg or (busy_reg and not pending_reg and scsi_ack);
 
-	-- Parity generation is multiplexed based on whether or not a transfer
-	-- is in progress and if so, the direction of the transfer. If no transfer
-	-- is in progress, the `data` lines feed the parity generator, allowing
-	-- its output to be latched with the rising edge of `sel`. Otherwise,
-	-- the parity generator is fed by the SCSI data bus.
-	parity_sel <= in_progress and direction;
 
-	parity_data <= data when parity_sel = '0' else
-	               scsi_db when parity_sel = '1' else
-	               "XXXXXXXX";
+	req_reg_next <= (not busy_reg and not pending_reg) or (not busy_reg and pending_reg and not scsi_ack) or (busy_reg and not pending_reg and not scsi_ack) or (busy_reg and pending_reg);
+	--req_reg_next <= not (not busy_reg and pending_reg and scsi_ack) or not (busy_reg and not pending_reg and scsi_ack);
+	--req_reg_next <= (not busy_reg and not pending_reg) or (pending_reg and not scsi_ack) or (busy_reg and not scsi_ack);
 
-	parity <= not (((parity_data(7) xor parity_data(6)) xor (parity_data(5) xor parity_data(4))) xor ((parity_data(3) xor parity_data(2)) xor (parity_data(1) xor parity_data(0))));
-
-	-- Edge detectors
-	sel_rising_edge_en <= not in_progress;
-	sel_rising_edge_rst <= rst or (in_progress and scsi_ack_rising_edge);
-	process(sel, sel_rising_edge_rst)
+	-- Data registers
+	process(clk)
 	begin
-		if sel_rising_edge_rst = '1' then
-			sel_rising_edge <= '0';
-		elsif rising_edge(sel) and sel_rising_edge_en = '1' then
-			sel_rising_edge <= '1';
-		end if;
-	end process;
-
-	scsi_ack_rising_edge_en <= in_progress;
-	scsi_ack_rising_edge_rst <= rst or (in_progress and scsi_ack_rising_edge);
-	process(scsi_ack, scsi_ack_rising_edge_rst)
-	begin
-		if scsi_ack_rising_edge_rst = '1' then
-			scsi_ack_rising_edge <= '0';
-		elsif rising_edge(scsi_ack) and scsi_ack_rising_edge_en = '1' then
-			scsi_ack_rising_edge <= '1';
-		end if;
-	end process;
-
-	scsi_ack_falling_edge_en <= in_progress;
-	scsi_ack_falling_edge_rst <= rst or (in_progress and scsi_ack_rising_edge);
-	process (scsi_ack, scsi_ack_falling_edge_rst)
-	begin
-		if scsi_ack_falling_edge_rst = '1' then
-			scsi_ack_falling_edge <= '0';
-		elsif falling_edge(scsi_ack) and scsi_ack_falling_edge_en = '1' then
-			scsi_ack_falling_edge <= '1';
-		end if;
-	end process;
-
-	in_progress_rst <= rst or (in_progress and scsi_ack_rising_edge);
-	process(sel_rising_edge, in_progress_rst)
-	begin
-		if in_progress_rst = '1' then
-			in_progress <= '0';
-			direction <= 'Z';
-
-			scsi_db <= "ZZZZZZZZ";
-			scsi_dbp <= 'Z';
-		elsif rising_edge(sel_rising_edge) then
-			in_progress <= '1';
-			direction <= rw;
-
-			if rw = '0' then
-				scsi_db <= data;
-				scsi_dbp <= parity;
-			else
-				scsi_db <= "ZZZZZZZZ";
-				scsi_dbp <= 'Z';
+		if rising_edge(clk) then
+			if input_write_enable = '1' then
+				input_data_reg <= data_in;
 			end if;
 		end if;
 	end process;
 
-	req_rst <= rst or (in_progress and scsi_ack_falling_edge);
-	process(sel_rising_edge, req_rst)
-	begin
-		if req_rst = '1' then
-			req <= '1';
-		elsif rising_edge(sel_rising_edge) then
-			req <= '0';
-		end if;
-	end process;
-
-	-- Output data and the `err` signal are latched on the falling edge
-	-- of `scsi_ack` if a read is in progress. Output data remains valid
-	-- until the next `rst` pulse, the next transfer, or the assertion
-	-- of `rw`.
-	data <= data_out when rw = '1' else "ZZZZZZZZ";
-	err <= err_out when rw = '1' else '0';
-
-	process(scsi_ack, rst)
+	process(rst, clk)
 	begin
 		if rst = '1' then
-			data_out <= "ZZZZZZZZ";
-			err_out <= '0';
-		elsif falling_edge(scsi_ack) then
-			if in_progress = '1' and direction = '1' then
-				data_out <= scsi_db;
-				err_out <= parity xor scsi_dbp;
+			output_data_reg <= X"00";
+			err_reg <= '0';
+		elsif falling_edge(clk) then
+			if output_write_enable = '1' then
+				output_data_reg <= scsi_db;
+				err_reg <= parity_err;
 			end if;
 		end if;
 	end process;
-end architecture;
 
+	-- Control registers
+	process(rst, clk)
+	begin
+		if rst = '1' then
+			direction_reg <= '1';
+		elsif rising_edge(clk) then
+			if input_write_enable = '1' then
+				direction_reg <= direction;
+			end if;
+		end if;
+	end process;
+
+	process(rst, clk)
+	begin
+		if rst = '1' then
+			pending_reg <= '0';
+		elsif rising_edge(clk) then
+			pending_reg <= pending_reg_next;
+		end if;
+	end process;
+
+	process(rst, clk)
+	begin
+		if rst = '1' then
+			busy_reg <= '0';
+			req_reg <= '1';
+		elsif falling_edge(clk) then
+			busy_reg <= busy_reg_next;
+			req_reg <= req_reg_next;
+		end if;
+	end process;
+
+	-- Parity generation
+	parity_data <= input_data_reg when direction_reg = '0' else scsi_db;
+	parity <= not (((parity_data(7) xor parity_data(6)) xor (parity_data(5) xor parity_data(4))) xor ((parity_data(3) xor parity_data(2)) xor (parity_data(1) xor parity_data(0))));
+	parity_err <= parity xor scsi_dbp;
+
+	-- Internal outputs
+	busy <= busy_reg;
+	err <= err_reg;
+	data_out <= output_data_reg;
+
+	-- SCSI outputs
+	scsi_req <= req_reg;
+	scsi_db <= (others => 'Z') when scsi_output_enable = '0' else input_data_reg;
+	scsi_dbp <= 'Z' when scsi_output_enable = '0' else parity;
+end architecture;
--- scsi_data_bus_io_tb.vhdl
+++ scsi_data_bus_io_tb.vhdl
@@ -1,175 +1,148 @@
---------------------------------------------------------------------------------
--- Company: 
--- Engineer:
---
--- Create Date:   13:40:53 04/02/2016
--- Design Name:   
--- Module Name:   C:/Data/dev/scsi/scsi_data_bus_io_tb.vhdl
--- Project Name:  scsi
--- Target Device:  
--- Tool versions:  
--- Description:   
--- 
--- VHDL Test Bench Created by ISE for module: scsi_data_bus_io
--- 
--- Dependencies:
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
---
--- Notes: 
--- This testbench has been automatically generated using types std_logic and
--- std_logic_vector for the ports of the unit under test.  Xilinx recommends
--- that these types always be used for the top-level I/O of a design in order
--- to guarantee that the testbench will bind correctly to the post-implementation 
--- simulation model.
---------------------------------------------------------------------------------
-LIBRARY ieee;
-USE ieee.std_logic_1164.ALL;
+library ieee;
+use ieee.std_logic_1164.all;
  
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---USE ieee.numeric_std.ALL;
+entity scsi_data_bus_io_tb is
+end scsi_data_bus_io_tb;
  
-ENTITY scsi_data_bus_io_tb IS
-END scsi_data_bus_io_tb;
- 
-ARCHITECTURE behavior OF scsi_data_bus_io_tb IS 
- 
-    -- Component Declaration for the Unit Under Test (UUT)
- 
-    COMPONENT scsi_data_bus_io
-    PORT(
-         rst : IN  std_logic;
-         sel : IN  std_logic;
-         rw : IN  std_logic;
-         busy : OUT  std_logic;
-         err : OUT  std_logic;
-         data : INOUT  std_logic_vector(7 downto 0);
-         scsi_req : OUT  std_logic;
-         scsi_io : OUT  std_logic;
-         scsi_ack : IN  std_logic;
-         scsi_db : INOUT  std_logic_vector(7 downto 0);
-         scsi_dbp : INOUT  std_logic
-        );
-    END COMPONENT;
-    
+architecture behavior of scsi_data_bus_io_tb is 
+	component scsi_data_bus_io
+		port(
+			-- internal bus signals
+			rst : in std_logic;
+			clk : in std_logic;
+			write_enable : in std_logic;
+			output_enable : in std_logic;
+			direction : in std_logic;
+			busy : out std_logic;
+			err : out std_logic;
+			data_in : in std_logic_vector(7 downto 0);
+			data_out : out std_logic_vector(7 downto 0);
 
-   --Inputs
-   signal rst : std_logic := '1';
-   signal sel : std_logic := '0';
-   signal rw : std_logic := '0';
-   signal scsi_ack : std_logic := '1';
+			-- scsi bus signals
+			scsi_req : out std_logic;
+			scsi_ack : in std_logic;
+			scsi_db : inout std_logic_vector(7 downto 0);
+			scsi_dbp : inout std_logic
+		);
+	end component;
+	 
+	signal rst : std_logic := '1';
+	signal clk : std_logic := '0';
+	signal write_enable : std_logic := '0';
+	signal output_enable : std_logic := '0';
+	signal direction : std_logic := '0';
+	signal busy : std_logic;
+	signal err : std_logic;
+	signal data_in : std_logic_vector(7 downto 0);
+	signal data_out : std_logic_vector(7 downto 0);
 
-	--BiDirs
-   signal data : std_logic_vector(7 downto 0);
-   signal scsi_db : std_logic_vector(7 downto 0);
-   signal scsi_dbp : std_logic;
+	signal scsi_req : std_logic;
+	signal scsi_ack : std_logic;
+	signal scsi_db : std_logic_vector(7 downto 0);
+	signal scsi_dbp : std_logic;
 
- 	--Outputs
-   signal busy : std_logic;
-   signal err : std_logic;
-   signal scsi_req : std_logic;
-   signal scsi_io : std_logic;
-BEGIN
- 
-	-- Instantiate the Unit Under Test (UUT)
-   uut: scsi_data_bus_io PORT MAP (
-          rst => rst,
-          sel => sel,
-          rw => rw,
-          busy => busy,
-          err => err,
-          data => data,
-          scsi_req => scsi_req,
-          scsi_io => scsi_io,
-          scsi_ack => scsi_ack,
-          scsi_db => scsi_db,
-          scsi_dbp => scsi_dbp
-        );
+	constant clk_period : time := 100 ns;
+begin
+	uut : scsi_data_bus_io
+		port map(
+			rst => rst,
+			clk => clk,
+			write_enable => write_enable,
+			output_enable => output_enable,
+			direction => direction,
+			busy => busy,
+			err => err,
+			data_in => data_in,
+			data_out => data_out,
 
-   -- Stimulus process
-   stim_proc: process
-   begin		
-      wait for 10 ns;	
+			-- scsi bus signals
+			scsi_req => scsi_req,
+			scsi_ack => scsi_ack,
+			scsi_db => scsi_db,
+			scsi_dbp => scsi_dbp
+		);
 
-		data <= "ZZZZZZZZ";
-		scsi_db <= "ZZZZZZZZ";
+	-- Clock process
+	process
+	begin
+		clk <= '0';
+		wait for clk_period / 2;
+		clk <= '1';
+		wait for clk_period / 2;
+	end process;
+
+	-- Stimulus process
+	stim_proc: process
+	begin
+		-- Set up SCSI bus
+		scsi_ack <= '1';
+		scsi_db <= (others => 'Z');
 		scsi_dbp <= 'Z';
+
+		-- Hold reset for 10 clocks
+		wait for clk_period * 10;
 		rst <= '0';
-		rw <= '0';
-		wait for 10 ns;
 
-		data <= "10100101";
-		wait for 2 ns;
-		sel <= '1';
-		wait for 1 ns;
-		--wait until busy = '1';
-		sel <= '0';
-		wait for 1 ns;
-		
-		--wait until scsi_req = '0';
+		-- Request a SCSI write
+		write_enable <= '1';
+		output_enable <= '1';
+		direction <= '0';
+		data_in <= X"AA";
+
+		wait until scsi_req = '0';
 		scsi_ack <= '0';
-		wait for 1 ns;
-		--wait until scsi_req = '1';
+		wait until scsi_req = '1';
 		scsi_ack <= '1';
-		wait for 1 ns;
-		--wait until busy = '0';
-		
-		data <= "ZZZZZZZZ";
-		rw <= '1';
-		sel <= '1';
-		wait for 1 ns;
-		--wait until busy = '1';
-		sel <= '0';
-		wait for 1 ns;
-		
-		--wait until scsi_req = '0';
-		scsi_db <= "01011010";
+
+		-- Request a SCSI read
+		write_enable <= '1';
+		output_enable <= '1';
+		direction <= '1';
+		scsi_db <= X"BB";
 		scsi_dbp <= '1';
-		wait for 2 ns;
-		scsi_ack <= '0';
-		wait for 1 ns;
-		--wait until scsi_req = '1';
-		scsi_ack <= '1';
-		wait for 1 ns;
-		--wait until busy = '0';
-		
-		sel <= '1';
-		wait for 1 ns;
-		sel <= '0';
-		wait for 1 ns;
-		
-		--wait until scsi_req = '0';
-		scsi_db <= "01011010";
-		scsi_dbp <= '0';
-		wait for 2 ns;
-		scsi_ack <= '0';
-		wait for 1 ns;
-		--wait until scsi_req = '1';
-		scsi_ack <= '1';
-		wait for 1 ns;
-		--wait until busy = '0';		
 
-		scsi_db <= "ZZZZZZZZ";
+		wait until scsi_req = '0';
+		scsi_ack <= '0';
+		wait until scsi_req = '1';
+		scsi_ack <= '1';
+
+		-- Request a SCSI write before the bus is free
+		write_enable <= '1';
+		output_enable <= '1';
+		direction <= '0';
+		data_in <= X"55";
+		scsi_ack <= '0';
+
+		wait for clk_period;
+		write_enable <= '0';
+
+		wait for clk_period * 3;
+		scsi_db <= (others => 'Z');
 		scsi_dbp <= 'Z';
-
-		data <= "11111111";
-		rw <= '0';
-		wait for 2 ns;
-
-		sel <= '1';
-		wait for 1 ns;
-		sel <= '0';
-		wait for 1 ns;
-		
-		scsi_ack <= '0';
-		wait for 1 ns;
 		scsi_ack <= '1';
-		wait for 1 ns;
 
-      wait;
-   end process;
+		wait until scsi_req = '0';
+		scsi_ack <= '0';
+		wait until scsi_req = '1';
 
-END;
+		-- Request a SCSI read before the bus is free
+		write_enable <= '1';
+		output_enable <= '1';
+		direction <= '1';
+
+		wait for clk_period;
+		write_enable <= '0';
+
+		wait for clk_period * 3;
+		scsi_db <= X"CC";
+		scsi_dbp <= '1';
+		scsi_ack <= '1';
+
+		wait until scsi_req = '0';
+		scsi_ack <= '0';
+		wait until scsi_req = '1';
+		scsi_ack <= '1';
+
+		wait;
+	end process;
+end;
